ccd9818a6e868e5b3e65c5ee78c5c59b3439e225
Erik Nilsson <erik@strawcitydesign.com>
2023-08-29 13:43:59 +0200

    Adding Toast component

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,13 @@
+import SignIn from "@/components/SignIn";
 import { buttonVariants } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { FC } from "react";
 
-const page: FC = ({}) => {
+const page: FC = () => {
   return (
     <div className="absolute inset-0">
-      <div className="h-full max-w-2xl mx-auto flex flex-col justify-center items-center gap-2">
+      <div className="h-full max-w-2xl mx-auto flex flex-col justify-center items-center gap-20">
         <Link
           href={"/"}
           className={cn(
@@ -16,7 +17,7 @@ const page: FC = ({}) => {
         >
           Home
         </Link>
-        {/* <SignIn/> */}
+        <SignIn />
       </div>
     </div>
   );
