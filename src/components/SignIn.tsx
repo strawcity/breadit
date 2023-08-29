aeddacf0a8e96a70b42da8192b5e20b0f3bad04d
Erik Nilsson <erik@strawcitydesign.com>
2023-08-29 10:35:07 +0200

    Initial commit with some starter code

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const SignIn = () => {
+  return (
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+      SignIn
+    </div>
+  );
+};
+
+export default SignIn;
